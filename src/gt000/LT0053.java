49257e779a4615d534d32e868708a98ca548d610
wangbo <wangbo@siebre.com>
2018-02-12 16:08:05 +0800

    beats 1.45%.

---
+++ src/gt000/LT0053.java
@@ -0,0 +1,82 @@
+package gt000;
+
+
+/**
+ * 53. Maximum Subarray
+ * */
+public class LT0053 {
+
+    public static void main(String[] args) {
+        
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        
+        nums = new int[]{-1};
+        
+        nums = new int[]{-2, -1};
+        
+        System.out.println(Lt0053(nums));
+    }
+    
+    public static int Lt0053(int[] nums)
+    {
+        int result = 0;
+        int i = 0;
+        int j = 0;
+        int len = nums.length;
+        int max = 0;
+
+        if(len == 1)
+        {
+            return nums[0];
+        }
+        if(len == 0)
+        {
+            return 0;
+        }
+        
+        len--;
+        while(len >= 0 && nums[len] < 0)
+        {
+            len--;
+        }
+        len++;
+        
+        if(len > 0)
+        {
+            for(i = 0; i < len; i++)
+            {
+                if(nums[i] <= 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    max = 0;
+                    for(j = i; j < len; j++)
+                    {
+                        max += nums[j];
+                        if(max > result)
+                        {
+                            result = max;
+                        }
+                    }
+                }
+            }
+        }
+        else
+        {
+            len = nums.length;
+            result = nums[0];
+            for(i = 0; i < len; i++)
+            {
+                if(nums[i] > result)
+                {
+                    result = nums[i];
+                }
+            }
+            
+        }
+        return result;
+    }
+
+}

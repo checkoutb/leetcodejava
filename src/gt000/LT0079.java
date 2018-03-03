d56a4493731c010de2d4a81fbfc25b2e8b6b546e
wb <f12.628@hotmail.com>
2018-03-03 15:12:14 +0800

    21ms, beats 29.1%

--- src/gt000/LT0079.java
+++ src/gt000/LT0079.java
@@ -15,13 +15,142 @@ public class LT0079 {
                 // word = "SEE", -> returns true,
                 // word = "ABCB", -> returns false.
         
+//        for(String word : words)
+//        {
+//            System.out.println(Lt0079(board, word));
+//        }
+        
+//        board = new char[][]{{'a'}};
+//        words = new String[]{"a"};
+
+        board = new char[][] { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } };
+        words = new String[] { "ABCCED" };
+        
+        board = new char[][] {{'a', 'a'}};
+        words = new String[] {"aaa"};
+        
+        LT0079 lt = new LT0079();
         for(String word : words)
         {
-            System.out.println(Lt0079(board, word));
+        	System.out.println(lt.Lt0079a(board, word));
         }
         
     }
 
+    // 21ms, beats 29.1%
+    public boolean Lt0079a(char[][] board, String word)
+    {
+    	char[] chArr = word.toCharArray();
+    	int lenI = board.length;
+    	int lenJ = board[0].length;
+    	char ch = chArr[0];
+    	char t;
+    	boolean result = false;
+    	
+    	for(int i = 0; i < lenI; i++)
+    	{
+    		for(int j = 0; j < lenJ; j++)
+    		{
+    			if(board[i][j] == ch)
+    			{
+    				t = ch;
+    				board[i][j] = (char) -1;
+    				result = RecursionB(board, chArr, 1, i, j);
+    				if(result)
+    				{
+    					return result;
+    				}
+    				board[i][j] = t;
+    			}
+    		}
+    	}
+    	return result;
+    }
+    
+    // 擦，是上下左右搜索，不是九宫格。
+    public boolean RecursionB(char[][] board, char[] chArr, int n, int i, int j)
+    {
+        if(n == chArr.length)
+        {
+            return true;
+        }
+        char ch = chArr[n];
+        int lenI = board.length;
+        int lenJ = board[0].length;
+        char t;
+        
+        lenI = i == (lenI - 1) ? i : i + 1;
+        lenJ = j == (lenJ - 1) ? j : j + 1;
+        int j2 = j > 0 ? j - 1 : 0;
+        int i2 = i > 0 ? i - 1 : 0;
+        int j3 = j;
+        int i3 = i;
+        
+        for(i3 = i2; i3 <= lenI; i3++)
+        {
+            if(board[i3][j] == ch)
+            {
+                t = ch;
+                board[i3][j] = (char) -1;
+                if (RecursionB(board, chArr, n + 1, i3, j))
+                {
+                    return true;
+                }
+                board[i3][j] = t;
+            }
+        }
+        
+        for(j3 = j2; j3 <= lenJ; j3++)
+        {
+            if(board[i][j3] == ch)
+            {
+                t = ch;
+                board[i][j3] = (char) -1;
+                if (RecursionB(board, chArr, n + 1, i, j3))
+                {
+                    return true;
+                }
+                board[i][j3] = t;
+            }
+        }
+        return false;
+    }
+    
+    public boolean RecursionA(char[][] board, char[] chArr, int n, int i, int j)
+    {
+    	if(n == chArr.length)
+    	{
+    		return true;
+    	}
+    	char ch = chArr[n];
+    	int lenI = board.length;
+    	int lenJ = board[0].length;
+    	char t;
+    	
+    	lenI = i == (lenI - 1) ? i : i + 1;
+    	lenJ = j == (lenJ - 1) ? j : j + 1;
+    	int j2 = j = j > 0 ? j - 1 : 0;
+    	
+    	for(i = i > 0 ? i - 1 : 0; i <= lenI; i++)
+    	{
+    		j = j2;
+    		for(; j <= lenJ; j++)
+    		{
+    			if(board[i][j] == ch)
+    			{
+    				t = ch;
+    				board[i][j] = (char) -1;
+    				if(RecursionA(board, chArr, n + 1, i, j))
+    				{
+    					return true;
+    				}
+    				
+    				board[i][j] = t;
+    			}
+    		}
+    	}
+    	return false;
+    }
     
     // ... 还是要递归。
     public static boolean Lt0079(char[][] board, String word)

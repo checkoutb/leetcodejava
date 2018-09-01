0b3ea6155f37618632396518e49d4024e3c19985
wb <294709574@qq.com>
2018-09-01 15:04:18 +0800

    9ms, almost fastest.

--- src/ge100/LT0114.java
+++ src/ge100/LT0114.java
@@ -16,11 +16,45 @@ public class LT0114
         
         new LT0114().Lt0114(head);
         
-        LTUtils.showTree(head, 3);
+        LTUtils.showTree(head, 6);
     }
-
+    
+    
+    // 看到一种解法：右子树优先的后序遍历。这种遍历方法处理的第一个点就是末结点，然后一个一个往上走。需要一个全局变量。
+    
+    // 9ms，almost fastest
     private void Lt0114(TreeNode root)
     {
+        if (root == null)
+        {
+            return;
+        }
+        TreeNode node = root;
+        preorder(node);
+        while (node.left != null)
+        {
+            node.right = node.left;
+            node.left = null;
+            node = node.right;
+        }
+    }
+    
+    private TreeNode preorder(TreeNode node)
+    {
+        TreeNode left = node.left;
+        TreeNode right = node.right;
+        TreeNode pre = node;
+        if (left != null)
+        {
+            pre = preorder(left);
+        }
         
+        if (right != null)
+        {
+            pre.left = right;
+            node.right = null;
+            pre = preorder(right);
+        }
+        return pre;
     }
 }
